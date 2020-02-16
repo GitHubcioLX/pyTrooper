59ab41bdca6f06538d4052be5c7cad85514acd76
bhuminiecki <blahy998@gmail.com>
2020-02-16 23:14:20 +0100

    Expanded error management

--- ErrorFormatter.py
+++ ErrorFormatter.py
@@ -2,7 +2,8 @@ class ErrorFormatter:
 
     errors = {
         "23505": "Istnieje już jednostka o takim identyfikatorze",
-        "22P02": "Identyfikator nie może być pusty oraz musi być liczbą"
+        "22P02": "Identyfikator nie może być pusty oraz musi być liczbą",
+        "22001": "Podany ciąg znaków jest za długi"
     }
 
     @staticmethod
