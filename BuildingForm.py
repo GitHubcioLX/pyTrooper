8baca43ab24ebcde01cafaea5425a3926e637e96
bhuminiecki <blahy998@gmail.com>
2020-02-16 22:28:46 +0100

    Added creation of buildings and refreshing of lists

--- BuildingForm.py
+++ BuildingForm.py
@@ -1,11 +1,15 @@
 import PyQt5
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from connector import Connector
 
 
 class BuildingForm(QWidget):
+    commited = pyqtSignal()
+
     def __init__(self, id_jednostki):
         super().__init__()
+        self.id_jednostki = id_jednostki
         self.setWindowTitle("Formularz (budynek)")
         self.layout = QFormLayout()
         self.set_form()
@@ -14,13 +18,23 @@ class BuildingForm(QWidget):
         self.layout.addRow(self.addButton)
         self.setLayout(self.layout)
 
+        self.addButton.clicked.connect(self.confirm)
+
     def set_form(self):
-        sign = QLineEdit()
-        self.layout.addRow("Oznaczenie: ", sign)
-        empCount = QLineEdit()
-        self.layout.addRow("Docelowa liczba personelu: ", empCount)
-        role = QLineEdit()
-        self.layout.addRow("Rola budynku: ", role)
+        self.sign = QLineEdit()
+        self.layout.addRow("Oznaczenie: ", self.sign)
+        self.empCount = QLineEdit()
+        self.layout.addRow("Docelowa liczba personelu: ", self.empCount)
+        self.role = QLineEdit()
+        self.layout.addRow("Rola budynku: ", self.role)
+
+    def confirm(self):
+        sign = self.sign.text()
+        empCount = self.empCount.text()
+        role = self.role.text()
+        Connector.insert_row("budynki", ["oznaczenie", "liczba_personelu", "rola_budynku", "id_jednostki"], [sign, empCount, role, self.id_jednostki])
+        self.commited.emit()
+        self.close()
 
 
 if __name__ == "__main__":
--- UnitManagement.py
+++ UnitManagement.py
@@ -1,17 +1,17 @@
-import PyQt5
-from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
+
+from BuildingForm import BuildingForm
 from Utilities import set_info_tab, create_table
 from connector import Connector
-from BuildingForm import BuildingForm
 
 # GUI
 # app = QApplication([])
 
 budynki = [["AB3", "Wychodek"], ["DG2", "Toaleta"], ["BG7", "Urynał"], ["QV1", "Ubikacja"], ["HU1", "Kibel"]]
-oficerowie = [["Stasiek", "Dupas", "9450245291"], ["Krzysiek", "Kasztan", "9150258790"], ["Wiechu", "Szachista", "5540235263"]]
-pojazdy = [["1", "Maluch"], ["2", "BMW"], ["3", "Silvia"], ["4", "Corolla"], ["5", "Rudy"], ["6", "Jagdpanzer"], ["7", "Rower"], ["8", "Turbosmiglowiec"]]
+oficerowie = [["Stasiek", "Dupas", "9450245291"], ["Krzysiek", "Kasztan", "9150258790"],
+              ["Wiechu", "Szachista", "5540235263"]]
+pojazdy = [["1", "Maluch"], ["2", "BMW"], ["3", "Silvia"], ["4", "Corolla"], ["5", "Rudy"], ["6", "Jagdpanzer"],
+           ["7", "Rower"], ["8", "Turbosmiglowiec"]]
 
 
 class UnitManagement(QTabWidget):
@@ -22,14 +22,39 @@ class UnitManagement(QTabWidget):
         self.setWindowTitle("Zarządzanie jednostką")
         self.setMinimumSize(400, 350)
         self.infoTab = set_info_tab(id_jednostki)
-        self.listTab1 = self.create_list_tab(["Oznaczenie", "Rola"], Connector.get_filtered("budynki", ["oznaczenie", "rola_budynku"], " WHERE id_jednostki = " + id_jednostki), "budynki")
-        self.listTab2 = self.create_list_tab(["ID", "Model"], Connector.get_filtered("pojazdy", ["id_pojazdu", "model"], " WHERE id_jednostki = " + id_jednostki), "pojazdy")
-        self.listTab3 = self.create_list_tab(["Imię", "Nazwisko", "PESEL"], Connector.get_filtered("oficerowie", ["imie", "nazwisko", "pesel"], " WHERE id_jednostki = " + id_jednostki), "oficerowie")
-        self.addTab(self.infoTab, "Ogólne")
-        self.addTab(self.listTab1, "Budynki")
-        self.addTab(self.listTab2, "Pojazdy")
-        self.addTab(self.listTab3, "Oficerowie")
+        self.insertTab(0, self.infoTab, "Ogólne")
+        self.listTab1 = None
+        self.refresh_buildings()
+        self.listTab2 = None
+        self.refresh_vechicles()
+        self.listTab3 = None
+        self.refresh_officers()
         self.addWindow = None
+
+    def refresh_buildings(self):
+        self.removeTab(1)
+        self.listTab1 = self.create_list_tab(["Oznaczenie", "Rola"],
+                                             Connector.get_filtered("budynki", ["oznaczenie", "rola_budynku"],
+                                                                    " WHERE id_jednostki = " + self.unit_id), "budynki")
+        self.insertTab(1, self.listTab1, "Budynki")
+        self.setCurrentIndex(1)
+
+    def refresh_vechicles(self):
+        self.removeTab(2)
+        self.listTab2 = self.create_list_tab(["ID", "Model"], Connector.get_filtered("pojazdy", ["id_pojazdu", "model"],
+                                                                                     " WHERE id_jednostki = " + self.unit_id),
+                                             "pojazdy")
+        self.insertTab(2, self.listTab2, "Pojazdy")
+        self.setCurrentIndex(2)
+
+    def refresh_officers(self):
+        self.removeTab(3)
+        self.listTab3 = self.create_list_tab(["Imię", "Nazwisko", "PESEL"],
+                                             Connector.get_filtered("oficerowie", ["imie", "nazwisko", "pesel"],
+                                                                    " WHERE id_jednostki = " + self.unit_id),
+                                             "oficerowie")
+        self.insertTab(3, self.listTab3, "Oficerowie")
+        self.setCurrentIndex(3)
 
     def create_list_tab(self, column_names, items, type):
         tab = QWidget()
@@ -52,7 +77,7 @@ class UnitManagement(QTabWidget):
         boxLayout.addWidget(editButton)
         buttons.setLayout(boxLayout)
 
-        #addButton.clicked.connect(show_add_windows(type, id))
+        # addButton.clicked.connect(show_add_windows(type, id))
         if type == "budynki":
             addButton.clicked.connect(self.add_building)
 
@@ -62,6 +87,7 @@ class UnitManagement(QTabWidget):
 
     def add_building(self):
         self.addWindow = BuildingForm(self.unit_id)
+        self.addWindow.commited.connect(self.refresh_buildings)
         self.addWindow.show()
 
 
