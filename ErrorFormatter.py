6f96624f35b16a13b01e11ed4a2f8e0abe4de997
bhuminiecki <blahy998@gmail.com>
2020-02-16 19:27:46 +0100

    Created error pop ups

--- ErrorFormatter.py
+++ ErrorFormatter.py
@@ -2,8 +2,9 @@ class ErrorFormatter:
 
     errors = {
         "23505": "Istnieje już jednostka o takim identyfikatorze",
+        "22P02": "Identyfikator nie może być pusty oraz musi być liczbą"
     }
 
     @staticmethod
     def get_error(code):
-        return ErrorFormatter.errors.get(code, "Nieznany błąd")
+        return ErrorFormatter.errors.get(code, "Nieznany błąd: " + code)
