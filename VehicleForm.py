e93c834e4151237d4adf748f09f9104ecb1da88e
GitHubcioLX <52084994+GitHubcioLX@users.noreply.github.com>
2020-02-19 23:22:57 +0100

    VehicleForm ready for edition

--- VehicleForm.py
+++ VehicleForm.py
@@ -1,16 +1,16 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-
 from connector import Connector
 
 
 class VehicleForm(QWidget):
     commited = pyqtSignal()
 
-    def __init__(self, id_jednostki, status):
+    def __init__(self, id_jednostki, status, id_pojazdu=None):
         super().__init__()
         self.id_jednostki = id_jednostki
         self.status = status
+        self.id_pojazdu = id_pojazdu
         self.setWindowTitle("Formularz (pojazd)")
         self.layout = QFormLayout()
         self.set_form()
@@ -25,20 +25,34 @@ class VehicleForm(QWidget):
     def set_form(self):
         self.rodzaj = QComboBox()
         self.rodzaj.addItems(Connector.get_enum("rodzaj_pojazdu_type"))
-        self.layout.addRow("Rodzaj: ", self.rodzaj)
         self.producent = QLineEdit()
-        self.layout.addRow("Producent: ", self.producent)
         self.model = QLineEdit()
-        self.layout.addRow("Model: ", self.model)
         self.masa = QLineEdit()
-        self.layout.addRow("Masa [kg]: ", self.masa)
         self.zaloga = QLineEdit()
-        self.layout.addRow("Liczba załogi: ", self.zaloga)
         self.zasieg = QLineEdit()
-        self.layout.addRow("Zasięg [km]: ", self.zasieg)
         self.rok = QLineEdit()
-        self.layout.addRow("Rok produkcji: ", self.rok)
         self.rejestracja = QLineEdit()
+        if self.id_pojazdu:
+            oldData = Connector.get_record("pojazdy", ["rodzaj", "producent", "model", "rok_produkcji", "masa",
+                                                       "liczba_zalogi", "zasieg", "rejestracja", "status"],
+                                           self.id_pojazdu, "id_pojazdu", int)
+            print(oldData)
+            self.rodzaj.setCurrentText(oldData[0])
+            self.producent.setText(oldData[1])
+            self.model.setText(oldData[2])
+            self.rok.setText(str(oldData[3]))
+            self.masa.setText(str(oldData[4]))
+            self.zaloga.setText(str(oldData[5]))
+            self.zasieg.setText(str(oldData[6]))
+            self.rejestracja.setText(oldData[7])
+            self.status = oldData[8]
+        self.layout.addRow("Rodzaj: ", self.rodzaj)
+        self.layout.addRow("Producent: ", self.producent)
+        self.layout.addRow("Model: ", self.model)
+        self.layout.addRow("Masa [kg]: ", self.masa)
+        self.layout.addRow("Liczba załogi: ", self.zaloga)
+        self.layout.addRow("Zasięg [km]: ", self.zasieg)
+        self.layout.addRow("Rok produkcji: ", self.rok)
         self.layout.addRow("Rejestracja: ", self.rejestracja)
 
     def confirm(self):
@@ -50,23 +64,32 @@ class VehicleForm(QWidget):
         zasieg = self.zasieg.text()
         rok = self.rok.text()
         rejestracja = self.rejestracja.text()
-        if(Connector.create_vehicle([rodzaj,
-                                  producent,
-                                  model,
-                                  masa,
-                                  zaloga,
-                                  zasieg,
-                                  self.status,
-                                  rok,
-                                  rejestracja,
-                                  self.id_jednostki,
-                                  None])):
-            self.commited.emit()
-            self.close()
+        id_zamowienia = None
+        if self.id_pojazdu:
+            if(Connector.update_row("pojazdy", ["rodzaj", "producent", "model", "rok_produkcji", "masa",
+                                                "liczba_zalogi", "zasieg", "rejestracja", "status"],
+                                    [rodzaj, producent, model, rok, masa, zaloga, zasieg, rejestracja, self.status],
+                                    self.id_pojazdu, "id_pojazdu", int)):
+                self.commited.emit()
+                self.close()
+        else:
+            if(Connector.create_vehicle([rodzaj,
+                                      producent,
+                                      model,
+                                      masa,
+                                      zaloga,
+                                      zasieg,
+                                      self.status,
+                                      rok,
+                                      rejestracja,
+                                      self.id_jednostki,
+                                      id_zamowienia])):
+                self.commited.emit()
+                self.close()
 
 
 if __name__ == "__main__":
     app = QApplication([])
-    window = VehicleForm("34529", "Dostępny")
+    window = VehicleForm('1', None, '1')
     window.show()
     app.exec_()
