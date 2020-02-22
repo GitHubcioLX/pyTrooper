0823b7c9463a8bc98b9cdcee96694b644b7e9b58
GitHubcioLX <52084994+GitHubcioLX@users.noreply.github.com>
2020-02-22 20:18:50 +0100

    OrderForm almost done

--- OrderForm.py
+++ OrderForm.py
@@ -13,11 +13,10 @@ class OrderForm(QWidget):
         self.id_jednostki = id_jednostki
         self.type = type
         self.setWindowTitle("Nowe zamówienie")
-        self.layout = QFormLayout()
+        self.layout = QVBoxLayout()
         self.set_form()
         self.addButton = QPushButton("Zatwierdź")
-        self.layout.addRow(QLabel())
-        self.layout.addRow(self.addButton)
+        self.layout.addWidget(self.addButton)
         self.setLayout(self.layout)
         self.setMinimumSize(250, 200)
 
@@ -27,53 +26,107 @@ class OrderForm(QWidget):
         #Projekt:
         #Pierwszy GroupBox "Zamówienie" - koszt i deadline
         #Drugi GroupBox "Pojazd"/"Wyposażenie"  - formularz tworzenia takowego
+        zamowienie = QGroupBox("Zamówienie")
+        z_box_layout = QFormLayout()
         self.koszt = QLineEdit()
-        self.koszt.setValidator(QIntValidator())
+        self.koszt.setValidator(QDoubleValidator(0.01, 999999.99, 2))
+        self.koszt.setMaxLength(9)
         self.data_zam = QLineEdit()
         self.data_zam.setInputMask("9999-99-99")
+        self.data_zam.setText(str(Connector.get_cur_date()))
         self.deadline = QLineEdit()
         self.deadline.setInputMask("9999-99-99")
+        z_box_layout.addRow("Data zamówienia: ", self.data_zam)
+        z_box_layout.addRow("Maksymalny termin: ", self.deadline)
+        z_box_layout.addRow("", QLabel("Format daty: <b>RRRR-MM-DD<\b>"))
+        z_box_layout.addRow("Koszt [zł]: ", self.koszt)
+        zamowienie.setLayout(z_box_layout)
+        self.layout.addWidget(zamowienie)
 
         if self.type == "ekwipunek":
-            self.ekwipunek = QComboBox()
-            self.eq = Connector.get_filtered("ekwipunek", ["typ", "producent", "model", "numer_seryjny"],
-                                             " WHERE status = 'Dostępny' ORDER BY typ, producent, model ASC")
-            eqCombo = []
-            for y in self.eq:
-                eqCombo.append(y[0] + ": " + y[1] + " " + y[2] + " [" + str(y[3]) + "]")
-            self.ekwipunek.addItems(eqCombo)
-            self.layout.addRow("Ekwipunek: ", self.ekwipunek)
+            ekwipunek = QGroupBox("Ekwipunek")
+            eq_box_layout = QFormLayout()
+            self.producent = QLineEdit()
+            self.producent.setMaxLength(20)
+            self.producent.setValidator(QRegExpValidator(QRegExp(rx)))
+            self.model = QLineEdit()
+            self.model.setMaxLength(20)
+            self.model.setValidator(QRegExpValidator(QRegExp(rx)))
+            self.data_produkcji = QLineEdit()
+            self.data_produkcji.setInputMask("9999-99-99")
+            self.data_waznosci = QLineEdit()
+            self.data_waznosci.setInputMask("9999-99-99")
+            self.typ = QComboBox()
+            self.typ.addItems(Connector.get_enum("rodzaj_wyposazenia_type"))
+            self.numer_seryjny = QLineEdit()
+            self.numer_seryjny.setValidator(QIntValidator())
+            eq_box_layout.addRow("Typ: ", self.typ)
+            eq_box_layout.addRow("Producent: ", self.producent)
+            eq_box_layout.addRow("Model: ", self.model)
+            eq_box_layout.addRow("Numer seryjny: ", self.numer_seryjny)
+            eq_box_layout.addRow("Data produkcji: ", self.data_produkcji)
+            eq_box_layout.addRow("Data ważności: ", self.data_waznosci)
+            eq_box_layout.addRow("", QLabel("Format daty: <b>RRRR-MM-DD<\b>"))
+            ekwipunek.setLayout(eq_box_layout)
+            self.layout.addWidget(ekwipunek)
         else:
-            self.pojazdy = QComboBox()
-            self.vh = Connector.get_filtered("pojazdy", ["producent", "model", "id_pojazdu"],
-                                             " WHERE status = 'Dostępny' AND id_jednostki = " + self.id_jednostki +
-                                             " ORDER BY producent, model ASC")
-            vhCombo = []
-            for y in self.vh:
-                vhCombo.append(y[0] + " " + y[1] + " [" + str(y[2]) + "]")
-            self.pojazdy.addItems(vhCombo)
-            self.layout.addRow("Pojazd: ", self.pojazdy)
-
-        self.layout.addRow("Data zamowienia: ", self.data_zam)
-        self.layout.addRow("Maksymalny termin: ", self.data_deadline)
-        self.layout.addRow("", QLabel("Format daty: <b>RRRR-MM-DD<\b>"))
+            pojazd = QGroupBox("Pojazd")
+            p_box_layout = QFormLayout()
+            self.rodzaj = QComboBox()
+            self.rodzaj.addItems(Connector.get_enum("rodzaj_pojazdu_type"))
+            self.producent = QLineEdit()
+            self.producent.setMaxLength(15)
+            self.producent.setValidator(QRegExpValidator(QRegExp(rx)))
+            self.model = QLineEdit()
+            self.model.setMaxLength(20)
+            self.model.setValidator(QRegExpValidator(QRegExp(rx)))
+            self.masa = QLineEdit()
+            self.masa.setValidator(QIntValidator())
+            self.zaloga = QLineEdit()
+            self.zaloga.setValidator(QIntValidator())
+            self.zasieg = QLineEdit()
+            self.zasieg.setValidator(QIntValidator())
+            self.rok = QLineEdit()
+            self.rok.setValidator(QIntValidator())
+            p_box_layout.addRow("Rodzaj: ", self.rodzaj)
+            p_box_layout.addRow("Producent: ", self.producent)
+            p_box_layout.addRow("Model: ", self.model)
+            p_box_layout.addRow("Masa [kg]: ", self.masa)
+            p_box_layout.addRow("Liczba załogi: ", self.zaloga)
+            p_box_layout.addRow("Zasięg [km]: ", self.zasieg)
+            p_box_layout.addRow("Rok produkcji: ", self.rok)
+            pojazd.setLayout(p_box_layout)
+            self.layout.addWidget(pojazd)
 
     def confirm(self):
-        data_od = self.data_od.text()
-        data_do = self.data_do.text()
-        oficer = self.oficerowie[self.oficer.currentIndex()][2]
+        data_zam = self.data_zam.text()
+        deadline = self.deadline.text()
+        koszt = self.koszt.text()
+        producent = self.producent.text()
+        model = self.producent.text()
         if self.type == "ekwipunek:":
-            ekwipunek = self.eq[self.ekwipunek.currentIndex()][3]
-            if(Connector.insert_row('"Przydzial-ekwipunek"', ["data_od", "data_do", "pesel_oficera", "numer_seryjny"],
+            numer_seryjny = self.numer_seryjny.text()
+            typ = self.typ.currentText()
+            data_produkcji = self.data_produkcji.text()
+            data_waznosci = self.data_waznosci.text()
+            if data_waznosci == "--":
+                data_waznosci = ""
+            if (Connector.insert_row('"Przydzial-ekwipunek"', ["data_od", "data_do", "pesel_oficera", "numer_seryjny"],
                                         [data_od, data_do, oficer, ekwipunek])):
                 if Connector.update_row("ekwipunek", ["status"], ["Przydzielony"], ekwipunek, "numer_seryjny", int):
                     self.commited.emit()
                     self.close()
         else:
-            pojazd = self.vh[self.pojazdy.currentIndex()][2]
-            if (Connector.insert_row('"Przydzial-pojazd"', ["data_od", "data_do", "pesel_oficera", "id_pojazdu"],
-                                     [data_od, data_do, oficer, pojazd])):
-                if Connector.update_row("pojazdy", ["status"], ["Przydzielony"], pojazd, "id_pojazdu", int):
+            rodzaj = self.rodzaj.currentText()
+            masa = self.masa.text()
+            zaloga = self.zaloga.text()
+            zasieg = self.zasieg.text()
+            rok = self.rok.text()
+            id = Connector.create_zamowienie_pojazd([koszt, data_zam, deadline])
+            print("ID: " + str(id))
+            if id:
+                if Connector.create_vehicle([rodzaj, producent, model, masa, zaloga, zasieg, "Zamówiony", rok, None,
+                                             self.id_jednostki, id]):
                     self.commited.emit()
                     self.close()
 
