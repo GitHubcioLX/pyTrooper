070e30b9b3ba66356c5330783a02a752b7d58ec9
GitHubcioLX <52084994+GitHubcioLX@users.noreply.github.com>
2020-02-23 14:12:55 +0100

    Added missing space

--- OrderManagement.py
+++ OrderManagement.py
@@ -35,7 +35,7 @@ class OrderManagement(QTabWidget):
         self.removeTab(1)
         self.listTab2 = self.create_list_tab(["ID zamówienia", "Data złożenia", "Koszt"],
                                              Connector.get_filtered('"Zamowienie-pojazd"', ["id_zamowienia", "data_zam", "koszt"],
-                                                                    " WHERE " + self.unit_id + "IN (SELECT p.id_jednostki FROM pojazdy p WHERE p.id_zamowienia = id_zamowienia)"
+                                                                    " WHERE " + self.unit_id + " IN (SELECT p.id_jednostki FROM pojazdy p WHERE p.id_zamowienia = id_zamowienia)"
                                                                     + " ORDER BY id_zamowienia ASC"),
                                              "pojazdy")
         self.insertTab(1, self.listTab2, "Pojazdy")
